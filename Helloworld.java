e5779fe2806c80c0a54e2b39116164247198b841
Pasindu Kaushan <pasindukaushan98@gmail.com>
2021-02-18 09:52:10 +0530

    first change

---
+++ Helloworld.java
@@ -0,0 +1,21 @@
+public class Helloworld {
+
+
+	public static void main (String [] args){
+		
+			System.out.println("Hello world");
+			int x=4;
+			int y=5;
+
+		    int z=x+y;
+		    System.out.println(z)
+		    System.out.println(z)
+		    
+
+
+	}
+
+
+
+
+}
---
+++ test21.py
@@ -0,0 +1,5 @@
+meters=float(input("Enter Your Value in meters : "))
+centi=meters*100
+print("Value in centimeters : "+str(centi)+"cm")
+
+print("pasindu")
